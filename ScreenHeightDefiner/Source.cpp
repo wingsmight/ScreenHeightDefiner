b98bb9f3993e70e04afd6b5b0c0dc58e12e9e19d
wingsmight <wingsmight@gmail.com>
2021-05-21 16:15:41 +0700

    check SSE3 support from DLL

--- ScreenHeightDefiner/Source.cpp
+++ ScreenHeightDefiner/Source.cpp
@@ -8,18 +8,26 @@ typedef int (*ImportFunction)(char*);
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 DWORD WINAPI ThreadFunc(void*);
+void DrawHeightLabel(HINSTANCE hInstance);
+void DrawHeightValue(HINSTANCE hInstance);
+void DrawSSE3SupportLabel(HINSTANCE hInstance);
+void DrawSSE3SupporValue(HINSTANCE hInstance);
 
 
 const int WINDOW_WIDTH = 500;
-const int WINDOW_HEIGHT = 150;
+const int WINDOW_HEIGHT = 300;
 
-const int LABEL_ID = 1;
-const int HEIGHT_LABEL_ID = 2;
+const int LABEL_LABEL_ID = 1;
+const int HEIGHT_VALUE_ID = 2;
+const int SSE3_SUPPORT_LABEL_ID = 3;
+const int SSE3_SUPPORT_VALUE_ID = 4;
 
 
 char screenHeightString[256];
+char sse3SupportString[10];
 HWND hwnd;
-HWND heightLabelHwnd;
+HWND heightValueHwnd;
+HWND sse3SupportValueHwnd;
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR cmdStr, int showCmd)
@@ -57,29 +65,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR cmdStr, i
 		return EXIT_FAILURE;
 	}
 
-	// label
-	DWORD labelStyle = WS_CHILD | WS_VISIBLE | SS_CENTER;
-	int labelWidth = WINDOW_WIDTH;
-	int labelHeight = 100;
-	HWND labelHwnd = CreateWindow("static", "Screen height:",
-		labelStyle,
-		0, 0, labelWidth, labelHeight,
-		hwnd, (HMENU)LABEL_ID, hInstance, NULL);
+	DrawHeightLabel(hInstance);
+	DrawHeightValue(hInstance);
 
-	HFONT labelFont = CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
-	SendMessage(labelHwnd, WM_SETFONT, WPARAM(labelFont), TRUE);
-
-	// height label
-	DWORD heightLabelStyle = WS_CHILD | WS_VISIBLE | SS_CENTER | SS_CENTERIMAGE;
-	int heightLabelWidth = WINDOW_WIDTH;
-	int heightLabelHeight = WINDOW_HEIGHT;
-	heightLabelHwnd = CreateWindow("static", screenHeightString,
-		heightLabelStyle,
-		0, 0, heightLabelWidth, heightLabelHeight,
-		hwnd, (HMENU)HEIGHT_LABEL_ID, hInstance, NULL);
-
-	HFONT heightLabelFont = CreateFont(50, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
-	SendMessage(heightLabelHwnd, WM_SETFONT, WPARAM(heightLabelFont), TRUE);
+	DrawSSE3SupportLabel(hInstance);
+	DrawSSE3SupporValue(hInstance);
 
 	ShowWindow(hwnd, showCmd);
 	UpdateWindow(hwnd);
@@ -120,7 +110,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		{
 			hDC = BeginPaint(hWnd, &paintStruct);
 
-			SetWindowText(heightLabelHwnd, screenHeightString);
+			SetWindowText(heightValueHwnd, screenHeightString);
+			SetWindowText(sse3SupportValueHwnd, sse3SupportString);
 
 			EndPaint(hWnd, &paintStruct);
 			break;
@@ -128,11 +119,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		case WM_CTLCOLORSTATIC:
 		{
 			int id = GetDlgCtrlID((HWND)lParam);
-			if (id == HEIGHT_LABEL_ID || id == LABEL_ID)
+			if (id == HEIGHT_VALUE_ID || id == LABEL_LABEL_ID)
 			{
 				HDC hdcStatic = (HDC)wParam;
-				SetTextColor(hdcStatic, RGB(25, 25, 25)); //gray
+				SetTextColor(hdcStatic, RGB(50, 50, 50)); // gray text
 				return (LRESULT)GetStockObject(NULL_BRUSH);
+			}
+			else if (id == SSE3_SUPPORT_LABEL_ID || id == SSE3_SUPPORT_VALUE_ID)
+			{
+				HDC hdcStatic = (HDC)wParam;
+				SetTextColor(hdcStatic, RGB(255, 255, 255)); // white text
+				SetBkMode(hdcStatic, TRANSPARENT); // remove text background
+				return (LRESULT)GetStockObject(DKGRAY_BRUSH);
 			}
 			break;
 		}
@@ -147,10 +145,98 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 DWORD WINAPI ThreadFunc(void*)
 {
-	HINSTANCE hinstLib = LoadLibrary(TEXT("ScreenHeightDefinerDLL.dll"));
-	ImportFunction dllFunc = (ImportFunction)GetProcAddress(hinstLib, "GetScreenHeight");
-	dllFunc(screenHeightString);
-	FreeLibrary(hinstLib);
+	HINSTANCE screenHeightLib = LoadLibrary(TEXT("ScreenHeight.dll"));
+	if (screenHeightLib != NULL)
+	{
+		ImportFunction dllFunc = (ImportFunction)GetProcAddress(screenHeightLib, "GetScreenHeight");
+		dllFunc(screenHeightString);
+
+		FreeLibrary(screenHeightLib);
+	}
+	else
+	{
+		MessageBox(hwnd, "Cannot load DLL!", "Error!", MB_OK | MB_ICONERROR);
+
+		ExitProcess(EXIT_FAILURE);
+		return EXIT_FAILURE;
+	}
+
+	HINSTANCE sse3SupportLib = LoadLibrary(TEXT("SSE3Support.dll"));
+	if (sse3SupportLib != NULL)
+	{
+		ImportFunction dllFunc = (ImportFunction)GetProcAddress(sse3SupportLib, "GetSSE3Status");
+		dllFunc(sse3SupportString);
+
+		FreeLibrary(sse3SupportLib);
+	}
+	else
+	{
+		MessageBox(hwnd, "Cannot load DLL!", "Error!", MB_OK | MB_ICONERROR);
+
+		ExitProcess(EXIT_FAILURE);
+		return EXIT_FAILURE;
+	}
 
 	return EXIT_SUCCESS;
+}
+
+void DrawHeightLabel(HINSTANCE hInstance)
+{
+	DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+	int width = WINDOW_WIDTH;
+	int height = 100;
+	HWND heightLabelHwnd = CreateWindow("static", "Screen height:",
+		style,
+		0, 0, width, height,
+		hwnd, (HMENU)LABEL_LABEL_ID, hInstance, NULL);
+
+	HFONT font = CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+	SendMessage(heightLabelHwnd, WM_SETFONT, WPARAM(font), TRUE);
+}
+void DrawHeightValue(HINSTANCE hInstance)
+{
+	DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER | SS_CENTERIMAGE;
+	int width = WINDOW_WIDTH;
+	int height = WINDOW_HEIGHT / 2;
+	heightValueHwnd = CreateWindow("static", screenHeightString,
+		style,
+		0, 0, width, height,
+		hwnd, (HMENU)HEIGHT_VALUE_ID, hInstance, NULL);
+
+	HFONT font = CreateFont(50, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+	SendMessage(heightValueHwnd, WM_SETFONT, WPARAM(font), TRUE);
+}
+void DrawSSE3SupportLabel(HINSTANCE hInstance)
+{
+	int bottomOffset = 28;
+
+	DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+	int posX = 0;
+	int posY = WINDOW_HEIGHT / 2 - bottomOffset;
+	int width = WINDOW_WIDTH;
+	int height = WINDOW_HEIGHT / 2 - bottomOffset;
+	HWND sse3SupportLabelHwnd = CreateWindow("static", "SSE3 support:",
+		style,
+		posX, posY, width, height,
+		hwnd, (HMENU)SSE3_SUPPORT_LABEL_ID, hInstance, NULL);
+
+	HFONT font = CreateFont(25, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+	SendMessage(sse3SupportLabelHwnd, WM_SETFONT, WPARAM(font), TRUE);
+}
+void DrawSSE3SupporValue(HINSTANCE hInstance)
+{
+	int bottomOffset = 28;
+
+	DWORD style = WS_CHILD | WS_VISIBLE | SS_CENTER;
+	int posX = 0;
+	int posY = WINDOW_HEIGHT * (0.75) - bottomOffset;
+	int width = WINDOW_WIDTH;
+	int height = WINDOW_HEIGHT / 2 - bottomOffset;
+	sse3SupportValueHwnd = CreateWindow("static", "1",
+		style,
+		posX, posY, width, height,
+		hwnd, (HMENU)SSE3_SUPPORT_LABEL_ID, hInstance, NULL);
+
+	HFONT font = CreateFont(50, 0, 0, 0, FW_DONTCARE, FALSE, FALSE, FALSE, ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_SWISS, "Arial");
+	SendMessage(sse3SupportValueHwnd, WM_SETFONT, WPARAM(font), TRUE);
 }
